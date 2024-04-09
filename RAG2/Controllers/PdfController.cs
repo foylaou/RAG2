18bd0800002fd23cee102b47395460869654d393
Foylaou <s225002731@gmail.com>
2024-04-09 23:28:31 +0800

    upload pdf

---
+++ RAG2/Controllers/PdfController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System;
+
+namespace RAG2.Controllers
+{
+    public class PdfController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Uploadpdf(IFormFile formData)
+        {
+            if (formData != null)
+            {
+                string guid = Guid.NewGuid().ToString();
+                // 獲取檔案名（包含副檔名）
+                // var fileName = Path.GetFileName(formData.FileName);
+                // 獲取副檔名
+                var fType = Path.GetExtension(formData.FileName.ToString());
+                // 定義檔保存路徑
+                var savePath = Path.Combine(Directory.GetCurrentDirectory(), "uploads", guid+fType);
+
+                // 確保目錄存在
+                Directory.CreateDirectory(Path.GetDirectoryName(savePath) ?? throw new InvalidOperationException());
+
+                // 保存檔到伺服器
+                await using (var fileStream = new FileStream(savePath, FileMode.Create))
+                {
+                    await formData.CopyToAsync(fileStream);
+                }
+                TempData["Message"] = "上傳成功";
+                return RedirectToAction("Index"); // 復位向到某個動作
+            }
+
+            TempData["Message"] = "沒有收到檔";
+            return RedirectToAction("Index");
+        }
+    }
+}
--- RAG2/Entities/__EFMigrationsHistory.cs
+++ RAG2/Entities/__EFMigrationsHistory.cs
@@ -5,7 +5,7 @@ namespace RAG2.Entities;
 
 public partial class __EFMigrationsHistory
 {
-    public string MigrationId { get; set; } = null!;
+    public string MigrationId { get; set; } = null;
 
-    public string ProductVersion { get; set; } = null!;
+    public string ProductVersion { get; set; } = null;
 }
